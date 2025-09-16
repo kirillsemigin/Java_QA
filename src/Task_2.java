3723f88bbc0d83fee784c05f1f454b4e20efff7b
kirillsemigin <kirillsemigin@mail.ru>
2025-09-16 21:02:32 +0300

    Задание 2

---
+++ src/Task_2.java
@@ -0,0 +1,10 @@
+public class Task_2 {
+    public static void main(String[] args) {
+        int age = 36;
+        String name = "My_name";
+        String lastname = "My_lastname";
+        System.out.println("Имя: " + name);
+        System.out.println("Фамилия: " + lastname);
+        System.out.println("Возраст: " + age);
+    }
+}
