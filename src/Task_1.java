f8c73a8ff7b108e08c605a51bd426c2c7e128de3
kirillsemigin <kirillsemigin@mail.ru>
2025-09-16 20:55:13 +0300

    выполнение домашнего задания. задание 1

--- src/Task_1.java
+++ src/Task_1.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Task_1 {
+    /*
+    Задача 1:
+Создать программу, которая будет выводить в консоли Ваше имя и фамилию.
+     */
+    public static void main(String[] args) {
+        String name = "Kirill";
+        String lastName = "Semigin";
+
+        System.out.println(name + ' ' + lastName);
+    }
 }
