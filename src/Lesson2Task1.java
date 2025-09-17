8c6a21855289cb1e93f13c279c04b3108f3920f4
kirillsemigin <kirillsemigin@mail.ru>
2025-09-17 21:24:49 +0300

    Урок 2 Практическое занятие 1 Номер месяца

---
+++ src/Lesson2Task1.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Lesson2Task1 {
+    /*
+    Задача 1:
+Ввести с консоли любое число от 1 до 12. В зависимости от введённого числа
+вывести в консоль соответствующую пору года по номеру месяца. При решении
+использовать switch.
+     */
+    public static void main(String[] args) {
+        System.out.println("Введите число месяца");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+
+        switch (n) {
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("February");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("June");
+                break;
+            case 7:
+                System.out.println("July");
+                break;
+            case 8:
+                System.out.println("August");
+                break;
+            case 9:
+                System.out.println("September");
+                break;
+            case 10:
+                System.out.println("October");
+                break;
+            case 11:
+                System.out.println("November");
+                break;
+            case 12:
+                System.out.println("December");
+                break;
+        }
+
+
+
+    }
+}
