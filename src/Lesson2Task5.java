0f141049c0e91b0b8dd7d4982b8e3dbfe5d99748
kirillsemigin <kirillsemigin@mail.ru>
2025-09-18 22:00:26 +0300

    Урок 2 Практическое занятие 5 Вывод температуры + проверка на пустой ввод и ввод нечислового значения

---
+++ src/Lesson2Task5.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Lesson2Task5 {
+    /*
+    Задача 2:
+Для введенного числа t (температура на улице) вывести: Если t>–5, то вывести
+«Warm».
+Если –5>= t >–20, то вывести «Normal». Если –20>= t, то вывести «Cold».
+     */
+    public static void main(String[] args) {
+        System.out.println("Введите температуру");
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        if (input.trim().isEmpty()) {
+            System.out.println("Вы ничего не ввели");
+            return;
+        }
+
+        try {
+            int t = Integer.parseInt(input);
+            if (t <= -20) {
+                System.out.println("Cold");
+            }
+            else if (t <= -5 && t > -20 ) {
+                System.out.println("Normal");
+            }
+            else {
+                System.out.println("Warm");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Вы ввели не число");
+        }
+    }
+}
