dc59b0d260e94e7dfa79e860e58f0d1c8ec0dfd1
kirillsemigin <kirillsemigin@mail.ru>
2025-09-18 20:28:31 +0300

    Урок 2 Практическое занятие 1 Добавлена проверка на случай если введено не число. и нотификация если введенное число вне диапозона

--- src/Lesson2Task1.java
+++ src/Lesson2Task1.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Lesson2Task1 {
@@ -10,49 +11,54 @@ public class Lesson2Task1 {
     public static void main(String[] args) {
         System.out.println("Введите число месяца");
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
 
 
-        switch (n) {
-            case 1:
-                System.out.println("January");
-                break;
-            case 2:
-                System.out.println("February");
-                break;
-            case 3:
-                System.out.println("March");
-                break;
-            case 4:
-                System.out.println("April");
-                break;
-            case 5:
-                System.out.println("May");
-                break;
-            case 6:
-                System.out.println("June");
-                break;
-            case 7:
-                System.out.println("July");
-                break;
-            case 8:
-                System.out.println("August");
-                break;
-            case 9:
-                System.out.println("September");
-                break;
-            case 10:
-                System.out.println("October");
-                break;
-            case 11:
-                System.out.println("November");
-                break;
-            case 12:
-                System.out.println("December");
-                break;
+        try {
+
+            int n = scanner.nextInt();
+
+            switch (n) {
+                case 1:
+                    System.out.println("January");
+                    break;
+                case 2:
+                    System.out.println("February");
+                    break;
+                case 3:
+                    System.out.println("March");
+                    break;
+                case 4:
+                    System.out.println("April");
+                    break;
+                case 5:
+                    System.out.println("May");
+                    break;
+                case 6:
+                    System.out.println("June");
+                    break;
+                case 7:
+                    System.out.println("July");
+                    break;
+                case 8:
+                    System.out.println("August");
+                    break;
+                case 9:
+                    System.out.println("September");
+                    break;
+                case 10:
+                    System.out.println("October");
+                    break;
+                case 11:
+                    System.out.println("November");
+                    break;
+                case 12:
+                    System.out.println("December");
+                    break;
+                default:
+                    System.out.println("Такого месяца не существует"); // Если введенное число находится вне диапозона выводим сообщение, что такого месяца не существует
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Вы ввели не число");
         }
-
-
-
     }
 }
