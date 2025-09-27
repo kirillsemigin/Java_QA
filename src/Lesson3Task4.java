5ca26be0c45aa75f81c562f2f0e5168e4cf54f68
kirillsemigin <kirillsemigin@mail.ru>
2025-09-27 21:36:35 +0300

    Урок 3 Практическое занятие 4 3.1 3.2 3.3 3.4 3.5 3.6

--- src/Lesson3Task4.java
+++ src/Lesson3Task4.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Lesson3Task4 {
@@ -5,47 +7,101 @@ public class Lesson3Task4 {
         System.out.println("Введите размер массива");
         Scanner scanner = new Scanner(System.in);
         int range = scanner.nextInt();
-        double[] numbers = new double[range];
+        int[] numbers = new int[range];
         scanner.close();
+        Random random = new Random();
 
-        for(int i = 0; i < range; i++){
-            numbers[i] = Math.random();
+        for (int i = 0; i < range; i++) {
+            numbers[i] = random.nextInt(3);
         }
         /*
         Задача 1:
 Пройти по массиву, вывести все элементы в прямом и в обратном порядке
          */
-        System.out.println("Вывод в консоль массива в прямом порядке");
-        for(int i = 0; i < range; i++){
-            System.out.println(numbers[i]);
+        System.out.println("Вывод массива в прямом порядке:");
+        for (int i = 0; i < range; i++) {
+            System.out.print(numbers[i] + " ");
         }
-        System.out.println("Вывод в консоль массива в обратном порядке");
-        for(int i = range -1; i >= 0; i--){
-            System.out.println(numbers[i]);
+        System.out.println();
+        System.out.println("Вывод массива в обратном порядке:");
+        for (int i = range - 1; i >= 0; i--) {
+            System.out.print(numbers[i] + " ");
         }
+        System.out.println(" ");
         /*
         Задача 2:
 Найти минимальный - максимальный элементы и вывести в консоль.
          */
-        double min = numbers[0];
-        for (int i = 0; i < range; i++){
+        int min = numbers[0];
+        for (int i = 0; i < range; i++) {
             if (numbers[i] < min) {
                 min = numbers[i];
             }
         }
+        int posMin = Arrays.binarySearch(numbers, min);
         System.out.println("Наименьшее число в массиве = " + min);
 
-        double max = numbers[0];
-        for (int i = 0; i < range; i++){
-            if (numbers[i] > max){
+        int max = numbers[0];
+        for (int i = 0; i < range; i++) {
+            if (numbers[i] > max) {
                 max = numbers[i];
             }
         }
+        int posMax = Arrays.binarySearch(numbers, max);
         System.out.println("Наибольшее число в массиве = " + max);
         /*
         Задача 3:
 Найти индексы минимального и максимального элементов и вывести в консоль.
          */
-
+        System.out.println("Индекс минимального элемента массива = " + posMin);
+        System.out.println("Индекс максимального элемента массива = " + posMax);
+        /*
+        Задача 4:
+Найти и вывести количество нулевых элементов. Если нулевых элементов нет -
+вывести сообщение, что их нет.
+         */
+        int counter = 0; // счетчик нулевых элементов
+        for (int i = 0; i < numbers.length; i++) { // перебираем массив
+            if (numbers[i] == 0) { //если находим элемент = 0 - увеличиваем счетчик
+                counter++;
+            }
+        }
+        if (counter == 0) { // если нулевых элементов нет и счетчик = 0 - выводим сообщение
+            System.out.println("Нулевых элементов нет");
+        } else { // если счетчик > 0 - выводим значение счетчика
+            System.out.println("Количество нулевых элементов = " + counter);
+        }
+        /*
+        Задача 5:
+Пройти по массиву и поменять местами элементы первый и последний, второй
+и предпоследний и т.д.
+         */
+        // меняем местами первый и последний элемент местами
+        int temp1 = numbers[0];
+        numbers[0] = numbers[range - 1];
+        numbers[range - 1] = temp1;
+        // меняем местами второй и предпоследний элемент местами
+        int temp2 = numbers[1];
+        numbers[1] = numbers[range - 2];
+        numbers[range - 2] = temp2;
+        // выводим на экран новое состояние массива
+        System.out.println(Arrays.toString(numbers));
+        /*
+        Задача 6:
+Проверить, является ли массив возрастающей последовательностью (каждое
+следующее число больше предыдущего).
+         */
+        boolean isSequence = true;
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < numbers[i -1]) {
+                isSequence = false;
+                break;
+            }
+        }
+        if (isSequence == true){
+            System.out.println("Массив является возрастающей последовательностью");
+        } else {
+            System.out.println("Массив не является возрастающей последовательностью");
+        }
     }
 }
