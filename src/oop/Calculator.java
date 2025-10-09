ed3cfa42f1b133acc3a46e6309b0cb92b1c98419
kirillsemigin <kirillsemigin@mail.ru>
2025-10-09 20:42:40 +0300

    Создана ветка lesson5 Доработан класс Calculator

--- src/oop/Calculator.java
+++ src/oop/Calculator.java
@@ -1,14 +1,29 @@
 package oop;
 /*
 Урок 6. ООП. Практика. Задача 1.
- */
-// 1.1
+1.1 Создать класс Calculator с полями weight, cost, color и заполнить их. И
+методами нахождения суммы, разности, деления, умножения двух чисел.
+Метод main в этом классе не создавать!
+*/
+
 public class Calculator {
     int weight;
     int cost;
     String color;
 
+    public int addition(int a, int b) {
+        return a + b;
+    }
 
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
 
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
 
+    public int division(int a, int b) {
+        return a / b;
+    }
 }
