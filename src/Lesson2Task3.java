3ef753a81d4a32496d112afda78eb470c6d51071
kirillsemigin <kirillsemigin@mail.ru>
2025-10-10 19:57:39 +0300

    update

--- src/Lesson2Task3.java
+++ src/Lesson2Task3.java
@@ -4,8 +4,8 @@ public class Lesson2Task3 {
 Используя for вывести каждое четное число от 2 до 20 включительно и больше 10
      */
     public static void main(String[] args) {
-        for(int n = 2; n < 21; n++) {
-            if(n % 2 == 0 && n > 10)
+        for (int n = 2; n < 21; n++) {
+            if (n % 2 == 0 && n > 10)
                 System.out.println(n);
 
         }
